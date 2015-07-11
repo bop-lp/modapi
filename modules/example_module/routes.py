a17f852307d88bf2ee27c2da806715aef38077e2
Christopher Su <christophersu9@gmail.com>
2015-07-11 00:07:30 -0700

    working

--- modules/example_module/routes.py
+++ modules/example_module/routes.py
@@ -1,14 +1,8 @@
-from flask import Blueprint, jsonify, abort
+from flask import Blueprint
 from config import config
 
 module = Blueprint(config['module_name'], __name__)
 
-counter = 0
-
 @module.route('/')
 def index():
-    try:
-        counter += 1
-        return jsonify({'counter': counter})
-    except:
-        abort(404)
+    return 'hello world'
--- server.py
+++ server.py
@@ -5,25 +5,27 @@ from flask import Flask
 
 import config
 
-app = Flask(__name__)
-
 def get_modules():
     return [x[0].replace('/', '.') for x in os.walk(config.MODULES_DIR)][1:]
 
-def load_modules(app):
-    for p in get_modules():
-        c = import_module(p + '.config')
-        k = config.MOD_CONFIG_ROUTES_MOD_KEY
-        rmod = c.config[k] if k in c.config else config.MOD_CONFIG_ROUTES_MOD_DEFAULT
-        m = import_module(p + '.' + rmod)
-        k = config.MOD_CONFIG_MOD_VAR_KEY
-        mvar = c.config[k] if k in c.config else config.MOD_CONFIG_MOD_VAR_DEFAULT
-        mod = getattr(m, mvar)
-        k = config.MOD_CONFIG_URL_PREFIX_KEY
-        pre = c.config[k] if k in c.config else None
-        app.register_blueprint(mod, url_prefix=pre)
+class ModApi:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.load_modules()
 
-load_modules(app)
+    def load_modules(self):
+        for p in get_modules():
+            c = import_module(p + '.config')
+            k = config.MOD_CONFIG_ROUTES_MOD_KEY
+            rmod = c.config[k] if k in c.config else config.MOD_CONFIG_ROUTES_MOD_DEFAULT
+            m = import_module(p + '.' + rmod)
+            k = config.MOD_CONFIG_MOD_VAR_KEY
+            mvar = c.config[k] if k in c.config else config.MOD_CONFIG_MOD_VAR_DEFAULT
+            mod = getattr(m, mvar)
+            k = config.MOD_CONFIG_URL_PREFIX_KEY
+            pre = c.config[k] if k in c.config else None
+            self.app.register_blueprint(mod, url_prefix=pre)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    api = ModApi()
+    api.app.run(debug=True)
