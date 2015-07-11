ba9680871c867d247479093610a68f472c016e24
Christopher Su <christophersu9@gmail.com>
2015-07-10 23:22:12 -0700

    get modules

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
+import imp
+import os
+
 from flask import Flask
+
+import config
 
 app = Flask(__name__)
 
-
+def get_modules():
+    return [x[0] for x in os.walk(config.MODULES_DIR)][1:]
 
 if __name__ == "__main__":
     app.run(debug=True)
