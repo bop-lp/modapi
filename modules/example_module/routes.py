5f28b7e00da550f0c34227cfe687c72c6f25ba8c
Christopher Su <christophersu9@gmail.com>
2015-07-11 18:10:25 -0700

    example

--- modules/example_module/routes.py
+++ modules/example_module/routes.py
@@ -7,7 +7,6 @@ module = Blueprint(config['module_name'], __name__)
 def index():
     if secret_key and secret_key_value:
         val = request.args.get(secret_key)
-        print val
         if val != secret_key_value:
             abort(403)
     return 'hello world'
