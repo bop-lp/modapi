642f707ee1cbe207972bf1b86af226d14cd63bf3
Christopher Su <christophersu9@gmail.com>
2015-07-17 09:13:12 -0700

    clenaup

--- server.py
+++ server.py
@@ -32,15 +32,16 @@ class ModApi:
     def load_modules(self):
         for p in get_modules():
             c = import_module(p + '.config')
+            mc = c.config
             k = config.MOD_CONFIG_ROUTES_MOD_KEY
-            rmod = c.config[k] if k in c.config else config.MOD_CONFIG_ROUTES_MOD_DEFAULT
+            rmod = mc[k] if k in mc else config.MOD_CONFIG_ROUTES_MOD_DEFAULT
             m = import_module(p + '.' + rmod)
             k = config.MOD_CONFIG_MOD_VAR_KEY
-            mvar = c.config[k] if k in c.config else config.MOD_CONFIG_MOD_VAR_DEFAULT
+            mvar = mc[k] if k in mc else config.MOD_CONFIG_MOD_VAR_DEFAULT
             mod = getattr(m, mvar)
-            inject(m, c.config)
+            inject(m, mc)
             k = config.MOD_CONFIG_URL_PREFIX_KEY
-            pre = c.config[k] if k in c.config else None
+            pre = mc[k] if k in mc else None
             self.app.register_blueprint(mod, url_prefix=pre)
 
 if __name__ == "__main__":
