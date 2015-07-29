2772b1a4b1ac512c1661bee5ddadac1ea64a5dd9
Christopher Su <christophersu9@gmail.com>
2015-07-28 22:04:12 -0700

    debug

--- server.py
+++ server.py
@@ -65,4 +65,4 @@ class ModApi:
 
 if __name__ == "__main__":
     api = ModApi()
-    api.app.run(debug=True, host=config.SERVER_HOST, port=config.SERVER_PORT)
+    api.app.run(debug=False, host=config.SERVER_HOST, port=config.SERVER_PORT)
