5d43849473ea4dfa383e3c38bf407899b1aaa338
Christopher Su <christophersu9@gmail.com>
2015-08-07 08:55:33 -0700

    Create gen_reqs.py

---
+++ gen_reqs.py
@@ -0,0 +1,17 @@
+import config
+
+import os
+
+# Generates requirements.txt file from modules
+found = ['server_requirements.txt']
+
+for f in os.listdir(config.MODULES_DIR):
+    path = os.path.join(config.MODULES_DIR, f)
+    if os.path.isdir(path):
+      path = os.path.join(path, 'requirements.txt')
+      if os.path.isfile(path):
+        found.append(path)
+
+with open('requirements.txt', 'w') as file:
+  for f in found:
+    file.write('-r %s\n' % f)
