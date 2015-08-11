50b386c1d5700858e66c90e5f16e5374f3d6f985
Christopher Su <christophersu9@gmail.com>
2015-08-10 22:47:24 -0700

    secrets

--- common.py
+++ common.py
@@ -3,6 +3,9 @@ from flask import abort, request
 
 import secrets
 
+MODAPI_SECRET_KEY = secrets.SECRET_KEY_VALUE
+MODAPI_BASE_URL = secrets.BASE_URL
+
 def secret_correct(val):
     return secrets.SECRET_KEY and secrets.SECRET_KEY_VALUE and \
         val == secrets.SECRET_KEY_VALUE
