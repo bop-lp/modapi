e81b87a8beac2a425e6ff07c1c534b1c16017027
Christopher Su <christophersu9@gmail.com>
2015-08-30 16:57:18 -0700

    proper ordering

--- pull_modules.sh
+++ pull_modules.sh
@@ -1,2 +1,2 @@
 cd modules
-find . -type d -not -name "example_module" -mindepth 1 -maxdepth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;
+find . -mindepth 1 -maxdepth 1 -type d -not -name "example_module" -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;
