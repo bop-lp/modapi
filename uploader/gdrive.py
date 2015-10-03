eba60fb85ef4dc87a4c4a729933e4d77d82c9805
Christopher Su <christophersu9@gmail.com>
2015-10-03 00:04:05 -0700

    fixed it

--- uploader/gdrive.py
+++ uploader/gdrive.py
@@ -1,33 +1,40 @@
 import datetime
 import json
-from subprocess import call
+import os
+import subprocess
 import tempfile
 
 import gdrive_config as config
 
 class GDriveUploader:
     def upload(self, filepath, title=None, parent=None):
-        command = '%s upload ' % config.GDRIVE_BINARY_PATH
+        command = '%s upload' % config.GDRIVE_BINARY_PATH
+        command += ' --file %s' % filepath
 
         if title:
-            command += '--title %s ' % title
+            command += ' --title %s' % title
         if parent:
-            command += '--parent %s ' % parent
+            command += ' --parent %s' % parent
 
-        command += '--file %s' % filepath
-
-        call(command, shell=True)
+        subprocess.call(command, shell=True)
 
     def quick_upload(self, obj, file_prefix=None, folder=None):
-        with tempfile.NamedTemporaryFile() as temp_file:
-            json.dump(obj, temp_file)
+        fd, temp_path = tempfile.mkstemp()
 
-            filename = '%s.json' % datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-            if file_prefix:
-                filename = '%s-%s' % (file_prefix, filename)
+        temp_file = open(temp_path, 'w')
+        json.dump(obj, temp_file)
 
-            self.upload(temp_file.name, title=filename, parent=folder)
+        filename = '%s.json' % datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+        if file_prefix:
+            filename = '%s-%s' % (file_prefix, filename)
+
+        temp_file.close()
+
+        self.upload(temp_file.name, title=filename, parent=folder)
+
+        os.close(fd)
+        os.remove(temp_path)
 
 if __name__ == '__main__':
     uploader = GDriveUploader()
-    uploader.upload('~/drive')
+    uploader.upload('test.json')
